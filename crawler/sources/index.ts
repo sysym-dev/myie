2525e889679dca2eb321778e6d6b817776f2637b
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-03-16 13:04:08 +0700

    remove

--- crawler/sources/index.ts
+++
@@ -1,6 +0,0 @@
-import { Kemenag } from './modules/kemenag';
-import { Source } from './source';
-
-export const sources: Record<string, new () => Source> = {
-  kemenag: Kemenag,
-};
--- src/modules/chapter/chapter.ts
+++
@@ -1,8 +0,0 @@
-export interface Chapter {
-  id: number;
-  arabicName: string;
-  latinName: string;
-  transliterationName: Record<string, any>;
-  totalVerse: number;
-  location: string;
-}
