d8fba53c3b5a787ce954d45186562cbc7b31b7f5
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-03-22 05:26:42 +0700

    feat: migrate

--- bin/migration.ts
+++ bin/migration.ts
@@ -1,12 +1,22 @@
+import 'dotenv/config';
 import { program } from 'commander';
 import { database } from '../src/core/database/database';
 import path from 'path';
+import { Knex } from 'knex';
+
+const config: Knex.MigratorConfig = {
+  directory: path.resolve(__dirname, '../src/database/migrations'),
+  extension: 'ts',
+};
 
 program.command('make <name>').action(async (name: string) => {
-  await database.migrate.make(name, {
-    directory: path.resolve(__dirname, '../src/database/migrations'),
-    extension: 'ts',
-  });
+  await database.migrate.make(name, config);
+});
+
+program.command('migrate').action(async () => {
+  await database.migrate.latest(config);
+
+  process.exit(0);
 });
 
 program.parse();
