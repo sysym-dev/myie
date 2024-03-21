2c3bccb2e5e59b6d423f591d62bc81c7bb412041
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-03-21 20:49:43 +0700

    feat: express flash

--- index.ts
+++ index.ts
@@ -1,11 +1,31 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
 import express from 'express';
+import session from 'express-session';
+import flash from 'express-flash-message';
 import { routes } from './src/routes';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'pug');
+
+app.use(
+  session({
+    secret: process.env.SECRET || 'secret',
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
+
+app.use(
+  flash({
+    sessionKeyName: 'flash',
+  }),
+);
+
+app.use(express.urlencoded({ extended: true }));
 
 routes.forEach((route) => {
   app.use(route);
