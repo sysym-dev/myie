cc616fa7630e8fbc3e9aa677a4856e351200be5e
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-03-23 05:47:41 +0700

    feat: read transactions

--- src/features/dashboard/dashboard.route.ts
+++ src/features/dashboard/dashboard.route.ts
@@ -1,9 +1,20 @@
 import { Router } from 'express';
+import { readTransactions } from '../transaction/transaction.service';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  return res.render('dashboard');
+router.get('/', async (req, res) => {
+  const transactions = await readTransactions({
+    sort: {
+      column: 'created_at',
+      direction: 'desc',
+    },
+    paginated: true,
+  });
+
+  return res.render('dashboard', {
+    transactions,
+  });
 });
 
 export { router as dashboardRoute };
--- src/features/transaction/transaction.service.ts
+++ src/features/transaction/transaction.service.ts
@@ -3,9 +3,43 @@ import { createTransactionSchema } from './schemas/create.schema';
 import { database } from '../../core/database/database';
 
 interface Transaction {}
+interface Paginated<T> {
+  rows: T[];
+  meta: {
+    count: number;
+  };
+}
 
 export async function createTransaction(
   data: z.infer<typeof createTransactionSchema>,
 ): Promise<Transaction> {
   return await database<Transaction>('transactions').insert(data);
+}
+
+export async function readTransactions(params?: {
+  start_at?: Date;
+  sort?: {
+    column: 'created_at';
+    direction: 'asc' | 'desc';
+  };
+  paginated?: boolean;
+}): Promise<Transaction[] | Paginated<Transaction>> {
+  const query = database<Transaction>('transactions');
+
+  if (params?.sort) {
+    query.orderBy(params.sort.column, params.sort.direction);
+  }
+
+  const rows = await query.select();
+
+  if (params?.paginated) {
+    return {
+      rows,
+      meta: {
+        count: await query.count(),
+      },
+    };
+  }
+
+  return rows;
 }
