911a4e1d20367dad890c9e15e50b2ce897b562a2
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-03-30 10:35:16 +0700

    feat: hide error in proudction

--- src/exceptions/server-error.exception.ts
+++ src/exceptions/server-error.exception.ts
@@ -4,6 +4,9 @@ export class ServerErrorException extends BaseException {
   name: string = 'Server Error';
 
   constructor(message: string) {
-    super(500, message);
+    super(
+      500,
+      process.env.NODE_ENV === 'production' ? 'Server Error' : message,
+    );
   }
 }
