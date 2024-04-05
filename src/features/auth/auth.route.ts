13fcb7d677b0fc75be461d09f79cf386a333a9ff
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-04-05 14:16:35 +0700

    feat: register

--- src/features/auth/auth.route.ts
+++ src/features/auth/auth.route.ts
@@ -4,6 +4,9 @@ import { validateSchema } from '../../middlewares/validate-schema.middleware';
 import { loginSchema } from './schemas/login.schema';
 import { checkValidLogin } from './auth.service';
 import { UnauthorizedException } from '../../exceptions/unauthorized.exception';
+import { registerSchema } from './schemas/register.schema';
+import { newUser } from '../user/user.service';
+import { DuplicateEntryException } from '../../exceptions/duplicate-entry.exception';
 
 const router = Router();
 
@@ -49,5 +52,27 @@ router.route('/register')
       title: 'Reister'
     })
   }))
+  .post(
+    validateSchema(registerSchema, { errorKey: 'register-error' }),
+    handleRequest(async (req, res) => {
+      try {
+        const user = await newUser({
+          email: req.body.email,
+          password: req.body.password
+        })
+  
+        req.session.userId = user.id
+        req.session.isLoggedIn = true
+  
+        return res.redirect('/')
+      } catch (err) {
+        if (err instanceof DuplicateEntryException) {
+          res.flash('register-error', err.message)
+
+          return res.redirect('/register')
+        }
+      }
+    })
+  )
 
 export { router as authRoute };
