383fb3b6f18bb0a0d8a72a2387f2e88cff9975f8
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-04-05 14:15:45 +0700

    feat: new user

--- src/features/user/user.service.ts
+++ src/features/user/user.service.ts
@@ -2,6 +2,9 @@ import { database } from '../../core/database/database';
 import { User } from './user';
 import { RecordNotFoundException } from '../../exceptions/record-not-found.exception';
 import { WithTransaction } from '../../core/database/transaction';
+import bcrypt from 'bcrypt'
+import { DuplicateEntryException } from '../../exceptions/duplicate-entry.exception';
+import { Count } from '../../core/database/count';
 
 export async function updateBalance(
   user: User,
@@ -38,4 +41,27 @@ export async function findByEmail(email: User['email']): Promise<User> {
   }
 
   return user;
+}
+
+export async function newUser(credential: {
+  email: User['email'],
+  password: User['password']
+}): Promise<User> {
+  if (await checkIsEmailExists(credential.email)) {
+    throw new DuplicateEntryException('email already exists')
+  }
+
+  const [id] = await database<User>('users').insert({
+    email: credential.email,
+    password: await bcrypt.hash(credential.password, 10)
+  })
+
+  return await findById(id)
+}
+
+export async function checkIsEmailExists(email: string): Promise<boolean> {
+  const [count] =
+    await database('users').where('email', email).count<Count>('* as count');
+
+  return !!count.count
 }
